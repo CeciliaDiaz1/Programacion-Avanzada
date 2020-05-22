51a5656115916e83567e4d2c29fe8f901fc610ec
CeciliaDiaz1 <60377230+CeciliaDiaz1@users.noreply.github.com>
2020-05-22 14:42:15 -0500

    PIA

---
+++ clasepia.py
@@ -0,0 +1,20 @@
+# Se importa la librería para tipo fecha.
+import datetime
+
+# Se define la clase Contacto.
+class Contacto:
+
+# Propiedades de la clase.
+  def __init__(self, nickname, nombre,correo,telefono,fechanacimiento,gastos):
+    # nickname = Nombre a traves del cual el contacto desea ser referido
+    self.nickname=nickname
+    # nombre = Nombre completo del usuario.
+    self.nombre=nombre
+    # correo = Correo del contacto.
+    self.correo=correo
+    # telefono = Tlefono del contacto.
+    self.telefono= telefono
+    #fechanacimiento = Fecha de nacimiento del contacto
+    self.fechanacimiento=fechanacimiento
+    #gastos = Gasto mensual del contacto
+    self.gastos=gastos
